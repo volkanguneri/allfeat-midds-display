c14683deb653969bbbb26762c09eef75b62a56ba
volkanguneri <guneriv@gmail.com>
2025-03-18 10:59:22 +0100

    config 2

--- sveltekit/svelte.config.js
+++ sveltekit/svelte.config.js
@@ -1,14 +1,31 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		postcss: {
+			configFileDirectory: '.'
+		},
+		style: ({ content, attributes }) => {
+			if (!attributes.lang || attributes.lang === 'postcss') {
+				return {
+					code: content,
+					dependencies: []
+				};
+			}
+			return null;
+		}
+	}),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '200.html'
+		}),
+		paths: {
+			base: '',
+			assets: ''
+		}
 	}
 };
 
